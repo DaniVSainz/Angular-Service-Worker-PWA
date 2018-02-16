7067433c89cdb8a89eec4b1a7ccc6602fe65feda
danivsainz <danivsainz@gmail.com>
2018-02-16 13:14:36 -0500

    Minor House keeping

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import Coffee from './logic/Coffee';
-
+import PlaceLocation from './logic/PlaceLocation';
 
 @Injectable()
 export class DataService {
@@ -10,8 +10,8 @@ export class DataService {
 
   getList(callback){
     const list = [
-      new Coffee('Double Espresso','Sunny Cafe', new PlaceLocation()),
-      new Coffee(),
+      new Coffee('Double Espresso','Sunny Cafe', new PlaceLocation("123 Market ST","San Francisco")),
+      new Coffee('Caramel Americano','StarCoffee', new PlaceLocation("Gran Via 34","Madrid")),
     ]
   }
 
--- src/app/logic/PlaceLocation.ts
+++ src/app/logic/PlaceLocation.ts
@@ -2,8 +2,8 @@ class PlaceLocation{
 
     constructor(public address:string="",
                 public city:string="",
-                public latitude:number,
-                public longitutde:number
+                public latitude:number= null,
+                public longitutde:number=null
     ){}
 
 }
