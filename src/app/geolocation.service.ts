d06d0d7c68597b5416e2bcbdb0313cba3ebb8396
danivsainz <danivsainz@gmail.com>
2018-02-16 12:55:00 -0500

    Impl geolocation service functions

--- src/app/geolocation.service.ts
+++ src/app/geolocation.service.ts
@@ -5,4 +5,35 @@ export class GeolocationService {
 
   constructor() { }
 
+
+  requestLocation(callback){
+    //W3C Geolocation API 
+    navigator.geolocation.getCurrentPosition(
+      //These are the two callbacks, if you check inside of getCurrentPosition help notes it shows them
+      position => {
+        callback(position.coords);
+      },
+      error => {
+        callback(null);
+      }
+    );
+  }
+
+  getMapLink(location){
+    let query = "";
+    if(location.latitude){
+      query = `${location.latitude} + , + ${location.longitude}`;
+    }else{
+      query = `${location.address} + , + ${location.city}`;
+    }
+
+    
+    if(/iPad|iPhone|iPod/.test(navigator.userAgent)){
+      return `https://maps.apple.com/?q=${query}`;
+    }else{
+      return `https://maps.google.com/?q=${query}`;
+    }
+
+  }
+
 }
