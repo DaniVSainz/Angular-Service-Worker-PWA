0312bb5ba701d18b2b600af449c05d7180d5aa03
danivsainz <danivsainz@gmail.com>
2018-02-17 12:37:37 -0500

    Create routing begin impl Coffee component features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+import {Routes,RouterModule} from '@angular/router';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule,MatIconModule,MatInputModule,MatSelectModule,MatSliderModule,MatToolbarModule,
         MatCardModule,MatSlideToggleModule} from '@angular/material';
 import 'hammerjs';
+
 import { ListComponent } from './list/list.component';
 import { CoffeeComponent } from './coffee/coffee.component'
+
+const routes: Routes = [
+  {path: '', component: ListComponent},
+  {path: 'coffee', component: CoffeeComponent},
+  {path: 'coffee/:id', component: CoffeeComponent}
+]
+
 
 @NgModule({
   declarations: [
@@ -19,6 +28,7 @@ import { CoffeeComponent } from './coffee/coffee.component'
     CoffeeComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,MatIconModule,MatInputModule,MatSelectModule,MatSliderModule,MatToolbarModule,
