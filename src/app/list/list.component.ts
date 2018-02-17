4976cc4f04bb4d9a2487849771f8f47e5d0ee38f
danivsainz <danivsainz@gmail.com>
2018-02-17 13:04:59 -0500

    Work in List component

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,6 @@
+import { DataService } from './../data.service';
 import { Component, OnInit } from '@angular/core';
+import Coffee from '../logic/Coffee';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private data: DataService) { }
+
+  list: [Coffee];
 
   ngOnInit() {
+    this.data.getList(list => {
+      this.list = list;
+    });
   }
 
 }
