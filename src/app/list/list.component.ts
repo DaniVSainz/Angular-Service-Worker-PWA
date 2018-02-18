5f451b8516cfe67fc67ef831a738d54ab2ee8621
danivsainz <danivsainz@gmail.com>
2018-02-18 06:24:19 -0500

    Finishing touches before converting to PWA

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,3 +1,4 @@
+import { GeolocationService } from './../geolocation.service';
 import { Router } from '@angular/router';
 import { DataService } from './../data.service';
 import { Component, OnInit } from '@angular/core';
@@ -11,7 +12,8 @@ import Coffee from '../logic/Coffee';
 export class ListComponent implements OnInit {
 
   constructor(private data: DataService,
-              private router: Router) { }
+              private router: Router,
+              private geolocationService:GeolocationService) { }
 
   list: [Coffee];
 
@@ -21,8 +23,30 @@ export class ListComponent implements OnInit {
     });
   }
 
-  goToDetails(coffee){
+  goToDetails(coffee:Coffee){
     this.router.navigate(['/coffee', coffee._id]);
+  }
+
+  goToMap(coffee:Coffee){
+    const mapUrl = this.geolocationService.getMapLink(coffee.location);
+    location.href = mapUrl;
+  }
+
+  shareCoffee(coffee:Coffee){
+    const shareText = `I had this coffee at ${coffee.place} and for me it's a ${coffee.rating} star coffee`;
+    let newVariable = (window.navigator as any)
+    if(newVariable.share){
+      newVariable.share({
+        title:coffee.name,
+        text: shareText,
+        url: window.location.href
+      }).then( () => {
+        console.log('Shared')
+      }).catch(() => console.log('error sharing'));
+    }else{
+      const shareURL = `whatsapp://send?text=${encodeURIComponent(shareText)}`;
+      location.href = shareURL;
+    }
   }
 
 }
